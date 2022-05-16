8f34a6d69835db5aef40cd75b3d3ba5920008bb7
cohohome <cohohome-frontend@ya.ru>
2022-05-16 04:30:03 +0300

    add all

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -30,4 +30,71 @@ document.addEventListener('DOMContentLoaded', function () {
       closePopup();
     }
   });
+
+  // Переключение на странице Автора
+  function toggleAuthorContent(e) {
+    var elms = document.querySelectorAll( '.toggle-mobile-content' );
+    var activeClass = 'tab--active';
+    var toggleId = Number(e.target.getAttribute('data-toggle'));
+
+    for ( var i = 0; i < elms.length; i++ ) {
+      elms[ i ].classList.remove(activeClass);
+    }
+    e.target.classList.add(activeClass);
+
+    if ( toggleId === 1 ) {
+      document.querySelector('.mobile-content-1').classList.add('mobile-content--show');
+      document.querySelector('.mobile-content-2').classList.remove('mobile-content--show');
+    } else {
+      document.querySelector('.mobile-content-1').classList.remove('mobile-content--show');
+      document.querySelector('.mobile-content-2').classList.add('mobile-content--show');
+    }
+  }
+
+  var btns = document.querySelectorAll('.toggle-mobile-content');
+  for ( var i = 0; i < btns.length; i++ ) {
+    btns[ i ].addEventListener('click', toggleAuthorContent);
+  }
+  if (btns[ 0 ]) {
+    btns[ 0 ].click();
+  }
+
+  // Попап материалов
+  function showPopupMaterial(e) {
+    var elms = document.querySelectorAll( '.material-popup' );
+    var activeClass = 'material-popup--show';
+    var popupId = Number(e.path[1].getAttribute('data-material-popup'));
+    console.log(e.path[1]);
+
+    for ( var i = 0; i < elms.length; i++ ) {
+      elms[ i ].classList.remove(activeClass);
+    }
+
+    document.querySelector('.material-popup[data-material-popup="' + popupId + '"]').classList.add(activeClass);
+    // document.querySelector('body').classList.add('overflow-hidden');
+  }
+
+  var materials = document.querySelectorAll('.trigger-popup-material');
+  for ( var i = 0; i < materials.length; i++ ) {
+    materials[ i ].addEventListener('click', showPopupMaterial);
+  }
+
+  function closePopupMaterial() {
+    var bgs = document.querySelectorAll('.material-popup');
+    for ( var i = 0; i < bgs.length; i++ ) {
+      bgs[ i ].classList.remove('material-popup--show');
+    }
+
+    // document.querySelector('body').classList.remove('overflow-hidden');
+  }
+
+  var bgs = document.querySelectorAll('.material-popup__bg');
+  for ( var i = 0; i < bgs.length; i++ ) {
+    bgs[ i ].addEventListener('click', closePopupMaterial);
+  }
+
+  var icloses = document.querySelectorAll('.material-popup__close');
+  for ( var i = 0; i < icloses.length; i++ ) {
+    icloses[ i ].addEventListener('click', closePopupMaterial);
+  }
 });
